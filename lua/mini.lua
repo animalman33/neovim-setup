72060c07856a174ce03879f4928db13bc05f1474
animalman33 <animalman3333@gmail.com>
2022-07-18 13:43:51 -0400

    backup

---
+++ lua/mini.lua
@@ -0,0 +1,27 @@
+require("mini.comment").setup({})
+
+require("mini.cursorword").setup({})
+
+require("mini.pairs").setup({})
+
+require("mini.surround").setup({
+	mappings = {
+		add = '<leader>sa', -- Add surrounding in Normal and Visual modes
+		delete = '<leader>sd', -- Delete surrounding
+		find = '<leader>sf', -- Find surrounding (to the right)
+		find_left = '<leader>sF', -- Find surrounding (to the left)
+		highlight = '<leader>sh', -- Highlight surrounding
+		replace = '<leader>sc', -- Replace surrounding
+		update_n_lines = '<leader>sn', -- Update `n_lines`
+	},
+})
+
+require("mini.tabline").setup({})
+
+require("mini.trailspace").setup({})
+
+-- require("mini.jump2d").setup({
+-- 	mappings = {
+-- 		start_jumping = '<leader>s'
+-- 	}
+-- })
